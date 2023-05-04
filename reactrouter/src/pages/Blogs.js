bf661a31eec87b4e4e5bc8249daf58375687e83c
AzizB283 <84971634+AzizB283@users.noreply.github.com>
2023-05-04 17:56:31 +0530

    Delete reactrouter directory

--- reactrouter/src/pages/Blogs.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-function Blogs(props) {
-  return (
-    <h1>{props.blogs}</h1>
-  )
-}
-
-export default Blogs
--- reactrouter/src/pages/Contact.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-function Contact(props) {
-  return (
-     <h1>{props.contact}</h1>
-  )
-}
-
-export default Contact
